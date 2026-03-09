6cc589852a280c80b9f7e784bad2aa9373ee30ef
Divyam Singh <divyamsingh1918@gmail.com>
2026-03-10 01:43:58 +0530

    core-wallet-registry-and-deploy-scripts

---
+++ script/Wallet.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "../src/Admin.sol";
+import "../src/Engineer.sol";
+import "../src/User.sol";
+import "../src/Organization.sol";
+
+contract Deploy is Script{
+    function run() external {
+        vm.startBroadcast();
+          UserWalletRegistry userwallet = new UserWalletRegistry();
+          EngineerWalletRegistry engineerWallet = new EngineerWalletRegistry();
+          AdminWalletRegistry adminWallet = new AdminWalletRegistry();
+          OrganizationWalletRegistry orgWallet = new OrganizationWalletRegistry();
+
+        vm.stopBroadcast();
+
+    }
+}
---
+++ src/User.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract UserWalletRegistry {
+    mapping(bytes32 => address) public accountWallet;
+    mapping(address=> bytes32) public walletAccount;
+
+    event WalletLinked(bytes32 accountId, address wallet);
+    event WalletRemoved(bytes32 accountId, address wallet);
+    function linkWallet(bytes32 accountId) external{
+        address oldWallet = accountWallet[accountId];
+        if(oldWallet != address(0)){
+            delete walletAccount[oldWallet];
+            emit WalletRemoved(accountId, oldWallet);
+        }
+        accountWallet[accountId] = msg.sender;
+        walletAccount[msg.sender] = accountId;
+        emit WalletLinked(accountId, msg.sender);
+    }
+    function removeWallet(bytes32 accountId) external{
+        address wallet = accountWallet[accountId];
+        require(wallet!= address(0),"No wallet linked");
+        delete walletAccount[wallet];
+        delete accountWallet[accountId];
+        emit WalletRemoved(accountId, wallet);
+    }
+    function getWallet(bytes32 accountId) external view returns (address){
+        return accountWallet[accountId];
+    }
+    function getAccount(address wallet) external view returns(bytes32){
+        return walletAccount[wallet];
+    }
+
+}
